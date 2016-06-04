21ed13d66256998d6f95cc731c4ffd6910a02c19
Gurjit Kaur <kgurjit@gmail.com>
2016-06-04 16:14:01 -0400

    Initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,88 @@
+var quiz = [
+	{country: 'Denmark', options:['Quito', 'Copenhagen', 'New Delhi', 'Prague'], capital: 'Copenhagen'},
+	{country: 'Germany', options:['Jakarta', 'Georgetown', 'Berlin', 'Oslo'], capital: 'Berlin'},
+	{country: 'South Korea', options:['Warsaw', 'Lima', 'Seol', 'Beijing'], capital: 'Seol'},
+	{country: 'Cuba', options:['Moscow', 'Paris', 'Budapest', 'Havana'], capital: 'Havana'},
+	{country: 'Ireland', options:['Dublin', 'Copenhagen', 'Tripoli', 'Prague'], capital: 'Dublin'}
+];
+
+var game = {};
+
+var resetScores = function(){
+	game.correctAnswers = 0;
+	game.wrongAnswers = 0;
+	game.unanswered = 0;
+	game.time = 30;
+};
+
+var getQuizHtml = function(){
+	var html = '';
+	for(var i=0;i<quiz.length;i++) {
+		var ques = quiz[i];
+
+		var quesHtml = '<div class="ques">';
+
+		quesHtml += '<h2>'+ ques.country + '</h2>';
+		for(var j=0;j<ques.options.length;j++){
+			var op = ques.options[j];
+			var id = 'c' + i + '_' + j;
+			quesHtml += '<input type="radio" name="country' + i + '" id="' + id + '" value="' + op + '"> <label for="' + id + '">' + op + '</label>';
+		}
+		quesHtml += '</div>';
+
+		html += quesHtml;
+	}
+	return html;
+};
+var updateAndShowScorecard = function(){
+	$("#correctAnswers").html(game.correctAnswers);
+	$("#wrongAnswers").html(game.wrongAnswers);
+	$("#unanswered").html(game.unanswered);
+	$(".scorecard").show();
+	$(".ques-section").hide();
+	$("#startBtn").show();
+};
+
+var updateScores = function(){
+	for(var i=0;i<quiz.length;i++) {
+		var ques = quiz[i];
+		var correctAnswer = ques.capital;
+		var answered = $('input:radio[name=country' + i + ']:checked').val();
+	
+	
+	if(typeof answered  === 'undefined') {
+			game.unanswered = game.unanswered + 1;
+		} else if(answered === correctAnswer) {
+			game.correctAnswers = game.correctAnswers + 1;
+		} else {
+			game.wrongAnswers = game.wrongAnswers + 1;
+		}
+	}
+	updateAndShowScorecard();
+};
+
+var updateTimeAndCheckAnswers = function(){
+	game.time = game.time - 1;
+	$("span#time").html(game.time);
+	if(game.time === 0) {
+		window.clearInterval(game.timer);
+		updateScores();
+	}
+};
+
+var startTimer = function(){
+	$("span#time").html(game.time);
+	game.timer = window.setInterval(updateTimeAndCheckAnswers, 1000);
+};
+
+//On click of start, display ques-section and hide start button
+$("#startBtn").on("click", function(){
+	$(".ques-section").show();
+	resetScores();
+	startTimer();
+	$(".scorecard").hide();
+	$("div.questions").html(getQuizHtml());	
+	$("#startBtn").hide();
+});
+
+
